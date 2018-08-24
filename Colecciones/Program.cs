84e50e402c6565f7b9896abdbcf6b5265f8cc1d6
lizba <lizba@DESKTOP-9Q44OD2>
2018-08-24 00:46:06 -0500

    Imprimir las materias

--- Colecciones/Program.cs
+++ Colecciones/Program.cs
@@ -11,38 +11,29 @@ namespace Colecciones
         static void Main(string[] args)
         {
 
-            List<Alumno> alumnos;
-            Alumno alumno = new Alumno();
-            alumno.Nombre = "Juan Perez";
-            alumno.Matricula = "1758964";
+            List<Alumno> alumnos = new List<Alumno>();
+            Alumno alumno1 = new Alumno();
 
-
-            alumno.alumnos[1].Nombre = "Maria Gonzalez";
-            alumno.alumnos[1].Matricula = "9475323";
-
-            alumno.alumnos[2].Nombre = "Lupe Castro";
-            alumno.alumnos[2].Matricula = "641565";
-
-            alumno.alumnos[3].Nombre = "Pepe Gamboa";
-            alumno.alumnos[3].Matricula = "146826";
-
-            alumno.alumnos[4].Nombre = "Concepción de la Rosa";
-            alumno.alumnos[4].Matricula = "758621";
-
-            Materia matematicas = new Materia();
-            matematicas.Nombre = "Matemáticas";
-            matematicas.Identificador = "M153222514";
-
+            alumno1.Nombre = "Jose Perez";
             //  Add es para añadir elementos
-            alumno.Materias.Add(new Materia());
-            alumno.Materias.Add(matematicas);
+            alumno1.Materias.Add(new Materia("Historia", "HST741"));
+            alumno1.Materias.Add(new Materia("Matematicas", "MTM789"));
+            alumno1.Materias.Add(new Materia("Civismo", "CVS756"));
 
-            alumno.Materias[0].Nombre = "Historia";
-            alumno.Materias[0].Identificador = "H416521";
+            //los alumnos de arriba se agregaron a la lista ya que estaban guardados en una variable
+            alumnos.Add(alumno1);
 
-            alumno.Materias.Add(new Materia());
-            alumno.Materias[2].Nombre = "Español";
-            alumno.Materias[2].Identificador = "E852455" ;
+            foreach (Alumno elemento in alumnos)
+            {
+                Console.WriteLine("Nombre: " + elemento.Nombre);
+                //El foreach Es para recorrer todos lo elementos de una coleccion o de un arreglo
+                foreach (Materia materia in elemento.Materias)
+                {
+                    Console.WriteLine("Materia: " + materia.Nombre);
+                }
+            }
+            Console.ReadLine();
+
 
             //cuando se remueven los elementos, la lista se recorre y el espacio 1 no queda vacio
            // alumno.Materias.RemoveAt(1);
@@ -50,16 +41,6 @@ namespace Colecciones
             //el clear borra todo en la lista
             //alumno.Materias.Clear();
 
-            Console.WriteLine("Alumno: " + alumno.Nombre);
-            Console.WriteLine("Número de materias: " + alumno.Materias.Count);
-
-            //El foreach Es para recorrer todos lo elementos de una coleccion o de un arreglo
-            foreach(Materia materia in alumno.Materias  Alumno Alumnos in alumno.Nombre)
-            {
-                Console.WriteLine("Materia: " + materia.Nombre);
-
-            }
-            Console.ReadLine();
         }
     }
 }
